3fab442901e901b669f95beb893fdac6fb3a9276
Rawand-abu-shammala <rwndsami@gmail.com>
2024-05-15 19:22:27 +0300

    Add awards component for project

--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -109,3 +109,26 @@ export const list = [
     type: "Apartment",
   },
 ]
+
+export const awards = [
+  {
+    icon: <i class='fa-solid fa-trophy'></i>,
+    num: "32 M",
+    name: "Blue Burmin Award",
+  },
+  {
+    icon: <i class='fa-solid fa-briefcase'></i>,
+    num: "43 M",
+    name: "Mimo X11 Award",
+  },
+  {
+    icon: <i class='fa-solid fa-lightbulb'></i>,
+    num: "51 M",
+    name: "Australian UGC Award",
+  },
+  {
+    icon: <i class='fa-solid fa-heart'></i>,
+    num: "42 M",
+    name: "IITCA Green Award",
+  },
+]
