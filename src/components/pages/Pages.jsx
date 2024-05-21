3c10f638f67c608d9c3950c7a81d9955f278c063
Rawand-abu-shammala <rwndsami@gmail.com>
2024-05-21 21:47:12 +0300

    Add about component for project

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -3,6 +3,8 @@ import Header from '../common/header/Header'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from '../home/Home'
 import Footer from '../common/footer/Footer'
+import About from '../about/About'
+
 
 
 const Pages = () => {
@@ -12,7 +14,7 @@ const Pages = () => {
      <Header />
     <Routes>
       <Route index path='/' Component={Home}/>
-    
+      <Route path='/about' Component={About}/>
 
     </Routes>
     <Footer />
